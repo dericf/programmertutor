f15eec61c7650b61f6f325b00b5d9c3eef94a52a
deric92 <dericgf@gmail.com>
2020-10-07 22:17:06 -0600

    change _app.js to app.tsx and wrap in AuthProvider

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/tailwind.css';
+import type { AppProps, AppContext } from 'next/app';
+import { AuthProvider } from '../hooks/useAuth';
+
+import NavBar from 'components/NavBar'
+
+function MyApp({ Component, pageProps }: AppProps): any {
+  return (
+    <AuthProvider>
+      <NavBar />
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
+
+export default MyApp;
