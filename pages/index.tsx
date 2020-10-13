7512f06c9bb671390c792d7051d663cabde5484c
deric92 <dericgf@gmail.com>
2020-10-12 22:45:17 -0600

    Implement Home Page Content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
+import { Divider } from '@/components/Divider';
+import { Footer } from '@/components/Footer';
+import { Hero } from '@/components/Hero';
+import { IntroCards } from '@/components/IntroCards';
+import { ServicesDescription } from '@/components/ServicesDescription';
+import { TopContactIcons } from '@/components/TopContactIcons';
 
 export default function Home() {
   return (
-    <div className='md:flex bg-white rounded-lg p-24 justify-center'>
-      <img
-        className='h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6'
-        src='<https://pbs.twimg.com/profile_images/1102120097081184257/61tO47TQ_400x400.jpg>'
-      />
-      <div className='text-center md:text-left'>
-        <h2 className='text-lg'>Jaasdasdsa Prins</h2>
-        <div className='text-blue-500'>JavaScript developer</div>
-        <div className='text-gray-600'>Twitter: @jakeprins_nl</div>
-        <div className='text-gray-600'>www.jakeprins.com</div>
-      </div>
+    <div className="pt__main_container md:container bg-white mx-auto p-0 m-0">
+      <TopContactIcons />
+
+      <Hero />
+
+      <Divider />
+
+      <IntroCards />
+
+      <Divider />
+
+      <ServicesDescription />
+
+      <Footer />
     </div>
   );
 }
