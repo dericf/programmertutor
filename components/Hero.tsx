2b51be2812e11d94c3b695250820c1b157486f90
deric92 <dericgf@gmail.com>
2020-10-12 22:43:36 -0600

    Add Hero Component

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Hero: React.FC = () => {
+  return (
+    <div className="pt__hero p-0 mx-12 my-12">
+      <h1 className="pt__title text-center text-5xl">Programmer Tutor</h1>
+      <h2 className="pt__subtitle text-center text-lg">
+        Fair Pricing. Flexible Schedule. Patient.
+      </h2>
+    </div>
+  );
+};
