79c2885196e32e5fc1ca4d4a25175f13b12cff3c
deric92 <dericgf@gmail.com>
2020-10-17 22:25:41 -0600

    Prevent form from being submitted over and over again.

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -10,6 +10,7 @@ type ContactForm = {
   helperText: string;
   isError: boolean;
   isSuccess: boolean;
+  isSubmitting: boolean;
 };
 
 type ContactFormErrors = {
@@ -32,6 +33,7 @@ const ContactForm = React.forwardRef((_, ref: any) => {
     helperText: '',
     isError: false,
     isSuccess: false,
+    isSubmitting: false,
   };
   const [form, setForm] = useState(defaultForm);
 
@@ -49,47 +51,51 @@ const ContactForm = React.forwardRef((_, ref: any) => {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    (window as any).grecaptcha.ready(async (_) => {
-      (window as any).grecaptcha
-        .execute('6LfNWNgZAAAAANv_nXCXGVtU0zfNHLEy---tKcb9', {
-          action: 'contactForm',
-        })
-        .then(async (token) => {
-          let data = {
-            name: form.name,
-            email: form.email,
-            course: form.course,
-            message: form.message,
-            token: token,
-          };
-          console.log('CAPTCHA??!! HELLO?');
-          let resp = await fetch('/api/contact-form', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-          });
-          const respData: any = await resp.json();
+    if (form.isSubmitting == false) {
+      form.isSubmitting = true;
+      (window as any).grecaptcha.ready(async (_) => {
+        (window as any).grecaptcha
+          .execute('6LfNWNgZAAAAANv_nXCXGVtU0zfNHLEy---tKcb9', {
+            action: 'contactForm',
+          })
+          .then(async (token) => {
+            let data = {
+              name: form.name,
+              email: form.email,
+              course: form.course,
+              message: form.message,
+              token: token,
+            };
+            console.log('CAPTCHA??!! HELLO?');
+            let resp = await fetch('/api/contact-form', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(data),
+            });
+            const respData: any = await resp.json();
 
-          if (respData.success) {
-            setForm({
-              ...form,
-              isError: false,
-              isSuccess: true,
-              helperText: `Success! I received your information. I'll be in touch with you shortly. Thanks!`,
-            });
-          } else {
-            console.log('ERROR: ');
-            setForm({
-              ...form,
-              isError: true,
-              helperText:
-                'There was a problem submitting the form. I apologize for that. Please Contact me via phone or email or try again later.',
-            });
-          }
-        });
-    });
+            if (respData.success) {
+              setForm({
+                ...defaultForm,
+                isError: false,
+                isSuccess: true,
+                helperText: `Success! I received your information. I'll be in touch with you shortly. Thanks!`,
+              });
+            } else {
+              console.log('ERROR: ');
+              setForm({
+                ...form,
+                isError: true,
+                helperText:
+                  'There was a problem submitting the form. I apologize for that. Please Contact me via phone or email or try again later.',
+              });
+            }
+            form.isSubmitting = false;
+          });
+      });
+    }
   };
 
   const handleChange: React.FormEventHandler = (
@@ -100,10 +106,13 @@ const ContactForm = React.forwardRef((_, ref: any) => {
   return (
     <>
       <h2 className="text-2xl sm:text-3xl text-center my-6 px-3">
-        Book a Free Consultation
+        Request a Free Consultation
       </h2>
 
-      <form className="w-full max-w-lg mx-auto px-6" ref={ref}>
+      <form
+        className="w-full max-w-lg mx-auto px-6"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <div className="flex flex-wrap -mx-3 mb-6 ">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
@@ -113,8 +122,9 @@ const ContactForm = React.forwardRef((_, ref: any) => {
               Name
             </label>
             <input
+              ref={ref}
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="name"
+              required
               type="text"
               name="name"
               placeholder="Enter your name"
@@ -135,7 +145,8 @@ const ContactForm = React.forwardRef((_, ref: any) => {
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               name="email"
-              type="text"
+              required
+              type="email"
               placeholder="Enter your email"
               value={form.email}
               onChange={handleChange}
@@ -183,17 +194,30 @@ const ContactForm = React.forwardRef((_, ref: any) => {
           <input
             type="button"
             value="Submit"
-            onClick={handleSubmit}
-            className="bg-blue-500 text-white px-6 py-2 rounded-md max-w-xs mx-6"
+            onClick={(e) => {
+              form.name.length != 0 && form.isSubmitting == false
+                ? handleSubmit(e)
+                : e.preventDefault();
+            }}
+            disabled={form.name.length == 0 || form.isSubmitting == true}
+            className={`font-bold text-white px-6 py-2 rounded-md max-w-xs mx-6 bg-blue-500  hover:bg-blue-dark focus:bg-blue-700 focus:border-white ${
+              form.name.length != 0 && form.isSubmitting == false
+                ? 'cursor-pointer'
+                : 'cursor-not-allowed'
+            }`}
           />
+
           <input
-            type="button"
             value="Cancel"
-            className="bg-red-500 text-white px-6 py-2 rounded-md  max-w-xs mx-6"
+            type="button"
             onClick={() => {
               setForm(defaultForm);
             }}
+            className={
+              'font-bold text-white px-6 py-2 rounded-md max-w-xs mx-6 bg-red-500 hover:bg-red-600 cursor-pointer'
+            }
           />
+
           <div
             className="g-recaptcha"
             data-sitekey={process.env.RECAPTCHA_SITE_KEY}
@@ -201,13 +225,13 @@ const ContactForm = React.forwardRef((_, ref: any) => {
           ></div>
         </div>
         {form.isError && (
-          <p className="text-red-600 text-md italic py-4 mx-auto">
+          <p className="text-red-600 text-md italic py-4 mx-auto text-center">
             {form.helperText}
           </p>
         )}
 
         {form.isSuccess && (
-          <p className="text-gray-800 text-md italic py-4 mx-auto">
+          <p className="text-gray-900 text-md italic py-4 mx-auto text-center">
             {form.helperText}
           </p>
         )}
