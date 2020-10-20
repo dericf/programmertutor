de219bf094ee37a41f75e77c4b70b464339fe8ea
deric92 <dericgf@gmail.com>
2020-10-19 21:00:10 -0600

    Add mx-auto to spinner to keep it centered

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,6 +1,6 @@
 function Spinner() {
   return (
-    <div className="spinner align-middle ">
+    <div className="spinner mx-auto align-middle ">
       <div className="bounce1 mr-1"></div>
       <div className="bounce2"></div>
       <div className="bounce3 ml-1"></div>
