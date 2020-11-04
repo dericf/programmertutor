0fd7f3f6f653c01c457f4679abf728e43121d105
deric92 <dericgf@gmail.com>
2020-11-03 20:55:14 -0700

    Add next.config.js for url redirects

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/booking',
+        destination: 'https://square.site/book/L8KGKG1182GQ7/programmer-tutor',
+        permanent: true,
+      },
+      {
+        source: '/bookings',
+        destination: 'https://square.site/book/L8KGKG1182GQ7/programmer-tutor',
+        permanent: true,
+      },
+      {
+        source: '/appointments',
+        destination: 'https://square.site/book/L8KGKG1182GQ7/programmer-tutor',
+        permanent: true,
+      },
+    ];
+  },
+};
