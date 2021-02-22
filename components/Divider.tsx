b0cc39585cb2ea7e7fac536ab4936459d36853b3
deric92 <dericgf@gmail.com>
2021-02-22 15:35:54 -0700

    Add props spreading to Divider component

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,3 +1,3 @@
-export const Divider: React.FC = () => {
-  return <div className="pt__divider bg-gray-400 h-3 w-100"></div>;
+export const Divider = (props) => {
+  return <div className="pt__divider bg-gray-400 h-3 w-100" {...props}></div>;
 };
