6584336ae57789ccaade49975bddd4e1461122a4
deric92 <dericgf@gmail.com>
2021-02-22 12:08:28 -0700

    Extend tailwind spacing values

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,20 @@ module.exports = {
   },
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '184': '46rem',
+        '208': '52rem',
+        '240': '60rem',
+      },
+    },
     container: {
       center: true,
     },
   },
   variants: {},
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
