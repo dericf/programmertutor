a01d46e938363f40fcbe8ec721d4cddc0df41ce9
deric92 <dericgf@gmail.com>
2021-02-22 14:31:00 -0700

    Change Prop Type

--- components/ServicesDescription.tsx
+++ components/ServicesDescription.tsx
@@ -1,15 +1,15 @@
 import PortableText from '@sanity/block-content-to-react';
-type ComponentProps = {
-  content: any;
+type Props = {
+  services: Array<any>;
 };
 
-export const ServicesDescription = ({ content }: ComponentProps) => {
+export const ServicesDescription = ({ services }: Props) => {
   return (
     <article className="prose prose-xl mt-6 mb-12 px-10 mx-auto max-w-5xl">
       <h2 className="text-2xl md:text-3xl text-center my-6 px-3">
         What can I help you with?
       </h2>
-      {content.services.map((service) => (
+      {services.map((service) => (
         <section className="mb-4" key={service._id}>
           <h3 className="text-sm md:text-lg font-bold">{service.title}</h3>
           <div>
