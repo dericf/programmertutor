4d1312ed64aa6218c0f25a9fc94fa7c429c609bf
deric92 <dericgf@gmail.com>
2021-02-22 14:28:44 -0700

    Minor fix to Hero image sizing

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
-import { croppedDimensions, urlFor } from '../lib/sanityImageBuilder';
+import { urlFor } from '../lib/sanityImageBuilder';
 import CallToActionButton from './CallToActionButton';
 interface Props {
   content?: any;
 }
 export const HeroImage = React.forwardRef(({ content }: Props, ref) => {
-  // const image = urlFor(content.mainBackgroundImage);
-  console.log('content.mainBackgroundImage :>> ', content.mainBackgroundImage);
   return (
-    <div className="grid mb-6 gap-y-0 md:gap-y-2">
-      <div className="relative w-screen mx-auto h-80 md:h-128 md:w-3/4 2xl:h-144 xl:w-3/4 2xl:w-3/4  ">
-        
+    <div className="grid mb-6 xl:my-6 gap-y-0 md:gap-y-6">
+      <div className="relative w-screen mx-auto h-80 md:h-128 2xl:h-144 xl:w-3/4 2xl:w-3/4  ">
         <Image
           className="shadow-lg"
           src={urlFor(content.mainBackgroundImage).url()}
@@ -21,7 +18,7 @@ export const HeroImage = React.forwardRef(({ content }: Props, ref) => {
           objectPosition="left center"
         />
       </div>
-        <CallToActionButton content={content} ref={ref} />
+      <CallToActionButton content={content} ref={ref} />
     </div>
   );
 });
