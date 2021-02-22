a68d4de5125b9167f2ab4ea38a0102b78406e182
deric92 <dericgf@gmail.com>
2021-02-22 14:31:29 -0700

    Remove meta tags from _document

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* PWA primary color */}
-          <meta name="theme-color" content="#000000" />
-          <meta
-            name="description"
-            content="Programmer/Software Tutoring Services. Affordable Computer Science Tutor."
-          />
+          <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" /> 
         </Head>
         <body>
           <Main />
