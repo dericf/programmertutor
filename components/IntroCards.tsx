aab1ee066bb60065df47214ff2ba6cdc247d7183
deric92 <dericgf@gmail.com>
2021-02-22 14:30:11 -0700

    Update intro cards styling

--- components/IntroCards.tsx
+++ components/IntroCards.tsx
@@ -1,43 +1,41 @@
 import PortableText from '@sanity/block-content-to-react';
 import { Divider } from './Divider';
 // import serializers from '../lib/sanityBlockText';
-type PropTypes = {
-  content: any;
+interface Props {
+  cards: any;
 };
 
-export const IntroCards = ({ content }: PropTypes) => {
+export const IntroCards = ({ cards }: Props) => {
   return (
-    <div className="pt__intro my-6">
-      <div className="flex flex-col lg:flex-row flex-wrap justify-around ">
-        {content.cards &&
-          content.cards.map((card) => {
-            return (
-              <div
-                className="pt__intro_card flex-grow flex-shrink-0 shadow-lg  rounded-lg p-5 m-6 max-w-md self-center lg:self-start prose prose-lg"
-                style={{ height: 'auto' }}
-                key={card._id}
-              >
-                <h2 className="text-2xl text-center leading-6 font-medium text-gray-900 mb-4">
-                  {card.title}
-                </h2>
+    <div className="my-6 grid grid-cols-1 md:grid-cols-2 align-middle justify-center">
+      {cards &&
+        cards.map((card) => {
+          return (
+            <div
+              className="flex-grow flex-shrink-0 shadow-lg  rounded-lg p-5 m-6 max-w-md self-start justify-self-center prose prose-lg"
+              style={{ height: 'auto' }}
+              key={card._id}
+            >
+              <h2 className="text-2xl text-center leading-6 font-medium text-gray-900 mb-4">
+                {card.title}
+              </h2>
 
-                {card.text && (
-                  // <p className="mt-2 text-base leading-6 text-gray-800">
-                  <div>
-                    <PortableText blocks={card.text} />
-                  </div>
-                  // </p>
-                )}
+              {card.text && (
+                // <p className="mt-2 text-base leading-6 text-gray-800">
+                <div>
+                  <PortableText blocks={card.text} />
+                </div>
+                // </p>
+              )}
 
-                {!card.text && (
-                  <p className="mt-2 text-base leading-6 text-gray-800">
-                    {card.description}
-                  </p>
-                )}
-              </div>
-            );
-          })}
-      </div>
+              {!card.text && (
+                <p className="mt-2 text-base leading-6 text-gray-800">
+                  {card.description}
+                </p>
+              )}
+            </div>
+          );
+        })}
     </div>
   );
 };
