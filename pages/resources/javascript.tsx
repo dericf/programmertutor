43afbff5e31c543b71844bc50ab61667454621d1
deric92 <dericgf@gmail.com>
2021-02-22 15:37:00 -0700

    Remove unused imports

--- pages/resources/javascript.tsx
+++ pages/resources/javascript.tsx
@@ -1,16 +1,4 @@
-import { Divider } from '@/components/Divider';
-import { Footer } from '@/components/Footer';
-import { Hero } from '@/components/Hero';
-import { IntroCards } from '@/components/IntroCards';
-import { ServicesDescription } from '@/components/ServicesDescription';
-
-import React, { useEffect } from 'react';
-import sanity from '../../lib/sanityClient';
 import Head from 'next/head';
-import ContactForm from '@/components/ContactForm';
-import { contentQuery } from '../../data/queries';
-
-import Resources from '@/components/ResourcesMain';
 import { NextPage } from 'next';
 import ResourcesJS from '@/components/ResourcesJS';
 
