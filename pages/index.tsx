a618e1369a7c0ea734cbbc875e9650d93293e2c6
deric92 <dericgf@gmail.com>
2021-02-22 14:30:52 -0700

    Rearrange home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Divider } from '@/components/Divider';
 import { Footer } from '@/components/Footer';
-import { Hero } from '@/components/Hero';
+import { HeroImage } from '@/components/HeroImage';
 import { IntroCards } from '@/components/IntroCards';
 import { ServicesDescription } from '@/components/ServicesDescription';
 
@@ -8,36 +8,42 @@ import React, { useEffect } from 'react';
 import sanity from '../lib/sanityClient';
 import Head from 'next/head';
 import ContactForm from '@/components/ContactForm';
-import { contentQuery } from '../data/queries';
+import { contentQuery, infoCardsQuery } from '../data/queries';
 
-export default function Home({ content, services }) {
+import { Layout } from '@/components/Layout';
+import { MainTitle } from '@/components/MainTitle';
+
+export default function Home({ content, infoCards }) {
   const contactFormRef = React.createRef();
 
   return (
-    <>
+    <Layout content={content}>
       <Head>
-        <title>Programmer Tutor | Fair. Flexible. Patient.</title>
+        <title>Programmer Tutor | Fair - Flexible - Patient</title>
+        <meta name="description" content="Affordable Computer Science tutoring services. Free consultation."/>
       </Head>
 
-      <Hero content={content} ref={contactFormRef} />
+      <MainTitle content={content} />
+      <HeroImage content={content} ref={contactFormRef} />
+      <IntroCards cards={infoCards} />
 
       {/* <Divider /> */}
 
-      <IntroCards content={content} />
-
       <Divider />
 
-      <ServicesDescription content={content} />
+      <ServicesDescription services={content.services} />
       <Divider />
       <ContactForm ref={contactFormRef} />
-    </>
+    </Layout>
   );
 }
 
 export const getStaticProps = async () => {
   const content = await sanity.fetch(contentQuery);
+  const infoCards = await sanity.fetch(infoCardsQuery);
+  // console.log(infoCards)
   return {
-    props: { content }, // will be passed to the page component as props
+    props: { content, infoCards }, // will be passed to the page component as props
     revalidate: 15,
   };
 };
