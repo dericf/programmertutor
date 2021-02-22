66afae6c49f951b0e39d25683e1e965c03d8b957
deric92 <dericgf@gmail.com>
2021-02-22 10:59:34 -0700

    Fix navbar icon styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,65 +3,48 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import HomeIcon from '@material-ui/icons/Home';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import PhoneIcon from '@material-ui/icons/Phone';
+
 export const NavBar = () => {
   const router = useRouter();
   return (
-    <div className="pt__navbar shadow-lg">
-      {router.pathname !== '/' && (
-        <div
-          className="fixed flex flex-row text-blue-700 cursor-pointer w-100 z-10"
-          style={{ top: 12, left: 12 }}
-        >
-          <Link href="/" as="/" title="Home">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              width="30"
-            >
-              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-            </svg>
+    <div className="pt__navbar shadow-lg bg-white">
+      <div className="fixed top-0 left-0 w-full flex flex-row flex-wrap text-blue-700 z-10 py-4 px-4 bg-white bg-opacity-100">
+        {router.pathname != '/' && (
+          <Link href="/" as="/">
+            <a className="w-auto px-4 py-1 border-none bg-white rounded-lg shadow-md mr-4 hover:bg-blue-400 hover:text-white">
+              Home
+            </a>
           </Link>
-        </div>
-      )}
+        )}
 
-      <div
-        className="fixed flex flex-row text-blue-700 cursor-pointer w-100 z-10"
-        style={{ top: 12, right: 12 }}
-      >
+        {/* {router.pathname != '/resources' && (
+          <Link href="/resources" as="/resources">
+            <a className="w-auto px-4 py-1 bg-white rounded-lg shadow-md hover:bg-blue-400 hover:text-white">
+              Resources
+            </a>
+          </Link>
+        )} */}
+
         <a
           type="tel"
           href="tel:7802382982"
-          className="mr-4"
+          className="mr-4 self-center ml-auto w-auto px-4 py-1 border-none bg-white rounded-lg shadow-md hover:bg-blue-400 hover:text-white"
           title="Phone: 7802382982"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            width="30"
-          >
-            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-          </svg>
+          <PhoneIcon />
         </a>
 
         <a
           type="email"
+          className="self-center w-auto px-4 py-1 bg-white rounded-lg border-none shadow-md hover:bg-blue-400 hover:text-white"
           href="mailto:contact@programmertutor.com"
           title="Email: contact@programmertutor.com"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            width="30"
-          >
-            <path
-              fillRule="evenodd"
-              d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <MailOutlineIcon />
         </a>
       </div>
     </div>
