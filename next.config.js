ce24b300c1c84cf6a356c11d4c964e729a2ac2e2
deric92 <dericgf@gmail.com>
2021-02-22 12:08:38 -0700

    Remove old redirects

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/booking',
-        destination: 'https://square.site/book/L8KGKG1182GQ7/programmer-tutor',
-        permanent: true,
-      },
-      {
-        source: '/bookings',
-        destination: 'https://square.site/book/L8KGKG1182GQ7/programmer-tutor',
-        permanent: true,
-      },
-      {
-        source: '/appointments',
-        destination: 'https://square.site/book/L8KGKG1182GQ7/programmer-tutor',
-        permanent: true,
-      },
-    ];
+  images: {
+    domains: ['cdn.sanity.io'],
   },
 };
