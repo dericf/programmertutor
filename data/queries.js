1671a9bfb2050cd2f485526d198ae306747f3582
deric92 <dericgf@gmail.com>
2021-02-22 14:32:36 -0700

    Fix sanity Query ordering

--- data/queries.js
+++ data/queries.js
@@ -5,16 +5,20 @@ export const contentQuery = `*[_type == "websiteContent"][0] {
   logoImage,
   callToActionButtonText,
   mainBackgroundImage,
-  "cards": *[ _type == "card"] | order(_createdAt) {
+  "cards": *[ _type == "card"] | order(displayOrder asc) {
     _id,
     title,
     description,
     text
-    },
-    "services": *[_type == "service"] | order(_createdAt) {
-      _id,
-      title,
-      description,
-    }
+  },
+  "services": *[_type == "service"] | order(displayOrder asc) {
+    _id,
+    title,
+    description,
+  }
 }
 `;
+
+export const infoCardsQuery = `*[_type == 'card'] | order(displayOrder asc)`
+
+// export const servicesQuery = `*[_type == 'service'] | order(displayOrder asc)`
